a430345c8fcb30ec1612daa46bd1d95094d04b28
Dante Piekart <dantepiekart@gmail.com>
2024-04-26 13:28:38 +0200

    Hello world!
    Expanded the "Hello world!".

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Hello world!")
+const express = require('express');
+const app = express();
+
+app.get('/', home).listen(3000);
+
+function home(req, res) {
+    res.send('<h1>Hello world!</h1>')
+}
