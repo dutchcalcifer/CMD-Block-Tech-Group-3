800d5ee75a899048c69b2af98c50698c5b0bea04
Dante Piekart <dantepiekart@gmail.com>
2024-05-13 16:00:25 +0200

    Login Checker
    - Added login validation
    - Added basic view structure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,40 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 
+// POST login
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (user) {
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (match) {
+        console.log("login succes!");
+        res.redirect("login");
+      } else {
+        console.log("Password incorect!");
+        res.redirect("login");
+      }
+    } else {
+      console.log("no user found!");
+      res.redirect("login");
+    }
+  } catch {
+    console.log("error");
+    res.redirect("login");
+  }
+};
+
+// POST register
+const newUser = async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({ ...req.body, password: hashedPassword });
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 // GET
 const getUsers = async (req, res) => {
   try {
@@ -13,19 +47,9 @@ const getUsers = async (req, res) => {
   }
 };
 
-// POST
-const newUser = async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await User.create({ ...req.body, password: hashedPassword });
-    res.redirect("/");
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 // export functions
 module.exports = {
+  loginUser,
   getUsers,
   newUser,
 };
