81050da405dce8dcec2f55c9e575d6d56925fdd8
AmberVenema <amber98.venema@gmail.com>
2024-05-27 02:22:17 +0200

    Add route and styling genre
    De route naar de genre pagina staat er en ik heb alvast wat styling aan de pagina toegevoegd

--- src/routes/route.js
+++ src/routes/route.js
@@ -82,5 +82,17 @@ router.get("/delete", userDelete);
 //  POST edit update 
 router.post("/edit", userUpdate) 
 
+
+// register-genres
+// register-genres page
+router.get("/register-genres", async (req, res) => {
+  try {
+    res.render("pages/register-genres");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
 // export route
 module.exports = router;
